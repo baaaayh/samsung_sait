ddebaa131b5172b66dc784e0c7584a7b3088f306
baaayh <baaaayh@gmail.com>
2024-05-10 18:23:43 +0900

    pc modified

--- src/assets/script/main.js
+++ src/assets/script/main.js
@@ -4,7 +4,7 @@ $(function () {
     let counterIdx = 0;
     let totalSection = $('.main-section').length - 1;
 
-    $(window).on('load resize', function () {
+    $(window).on('resize', function () {
         ww = $(window).innerWidth();
     });
 
@@ -82,7 +82,7 @@ $(function () {
                     $body.addClass('scroll-lock');
                     $(window).scrollTop(0, 0);
                 }
-                $('.indicator li').eq(currentIndex).addClass('active').siblings('li').removeClass('active');
+                $('.indicator li').eq(currentIndex).addClass('active').find('span').show().siblings('li').removeClass('active');
             },
             // 섹션 넘기기
 
@@ -103,6 +103,7 @@ $(function () {
                 setTimeout(function () {
                     isMove = false;
                 }, 1200);
+
                 this.setCurrentSection();
             },
             setCurrentSection: function () {
@@ -111,14 +112,13 @@ $(function () {
                 if (currentIndex <= totalSection) {
                     $('.indicator li').eq(currentIndex).addClass('active').siblings('li').removeClass('active');
                 }
-                console.log(currentIndex);
 
                 // section active
                 $('.main-section').removeClass('active');
                 currentSection.addClass('active').siblings('.main-section').removeClass('active');
                 $('.main-section').find('.aos-init').removeClass('aos-animate');
                 $('.main-section.active').find('.aos-init').addClass('aos-animate');
-
+                $('.indicator li.active span').slideDown(700).parents('li').siblings('li').find('span').slideUp(700);
                 //currentSection.siblings('.main-section').find('.aos-init').removeClass('aos-animate');
                 // 섹션 컬러라이징
                 if (currentSection.data('color') === 'white') {
@@ -206,14 +206,14 @@ $(function () {
                 }
             },
             keyDownHandler: function (e) {
-                console.log(e.originalEvent.key);
                 if (e.originalEvent.key === 'ArrowDown') {
                     this.setTransitionSection(currentIndex + 1);
                 } else if (e.originalEvent.key === 'ArrowUp') {
                     this.setTransitionSection(currentIndex - 1);
+                } else {
+                    let idx = e.originalEvent.key - 1;
+                    this.setTransitionSection(idx);
                 }
-                let idx = e.originalEvent.key - 1;
-                this.setTransitionSection(idx);
             },
             init: function () {
                 this.isOverFlow();
@@ -221,7 +221,6 @@ $(function () {
                 $(window).on('wheel scroll', (e) => this.handleWheel(e));
                 $(window).on('touchstart', (e) => this.handleTouch(e));
                 $(window).on('touchend', (e) => this.handleTouchMove(e));
-                $(window).on('load', () => this.isOverFlow());
                 $(window).on('resize', () => this.isOverFlow());
                 $(window).on('scroll touchmove', () => this.handleMobileScroll());
                 $('.scroll-box').on('click', () => this.scrollBtnHandler());
@@ -235,18 +234,18 @@ $(function () {
 
     const main = {
         mainKvInit() {
-            const mainKV = new Swiper('.main-kv', {
+            let mainKV = new Swiper('.main-kv', {
                 autoplay: {
                     delay: 7000,
                     disableOnInteraction: false,
                 },
-                speed: 1500,
+
                 loop: true,
                 pagination: {
                     el: '.main-kv__control',
                     clickable: true,
                     renderBullet: function (index, className) {
-                        return '<li class="' + className + '"><button type="button" >' + (index < 9 ? '0' : '') + (index + 1) + '</button><div class="main-kv__bg"><span class="main-kv__bar"></span></div></li>';
+                        return '<li class="' + className + '"><span type="button" >' + (index < 9 ? '0' : '') + (index + 1) + '</span><div class="main-kv__bg"><span class="main-kv__bar"></span></div></li>';
                     },
                 },
                 effect: 'fade',
@@ -254,10 +253,78 @@ $(function () {
                 hashNavigation: {
                     watchState: true,
                 },
+                watchSlidesProgress: true,
+                touchRatio: 0,
+                on: {
+                    autoplayTimeLeft(s, time, progress) {
+                        $('.main-kv__bar').css({ width: (1 - progress) * 100 + '%' });
+                    },
+                },
             });
+        },
+        sliderInit() {
+            const researchArr = ['AI Algorithms', 'Computing Platform', 'Human Augmentation', 'Metaphotonics', 'Nano Electronics', 'Display Materials', 'Battery Materials', 'Semiconductor Materials', 'Environmental Research'];
+            const bgSlider = new Swiper('.bg-slider', {
+                autoplay: false,
+                loop: true,
+                effect: 'fade',
+                fadeEffect: { crossFade: true },
+                hashNavigation: {
+                    watchState: true,
+                },
+                touchRatio: 0,
+                pagination: {
+                    el: '.research__list',
+                    clickable: true,
+                    renderBullet: function (index, className) {
+                        return '<li class="' + className + '"><span>' + (index < 9 ? '0' : '') + (index + 1) + '</span><button type="button">' + researchArr[index] + '</button></li>';
+                    },
+                },
+                speed: 1200,
+            });
+            const textSlider = new Swiper('.text-slider', {
+                autoplay: false,
+                loop: true,
+                effect: 'fade',
+                fadeEffect: { crossFade: true },
+            });
+            const imageSlider = new Swiper('.image-slider', {
+                autoplay: false,
+                loop: true,
+            });
+            bgSlider.controller.control = [textSlider, imageSlider];
+
+            const pressSlider = new Swiper('.press__slider', {
+                slidesPerView: 2,
+                autoplay: {
+                    delay: 7000,
+                    disableOnInteraction: false,
+                },
+                loop: true,
+                hashNavigation: {
+                    watchState: true,
+                },
+                navigation: {
+                    nextEl: '.press__button--next',
+                    prevEl: '.press__button--prev',
+                },
+                speed: 1200,
+                on: {
+                    init(v) {
+                        main.handleProgress(v);
+                    },
+                },
+            });
+            pressSlider.on('slideChange', (v) => this.handleProgress(v));
+        },
+        handleProgress(event) {
+            const total = event.slides.length;
+            const curr = event.realIndex + 1;
+            $('.press__bar').css({ width: (curr / total) * 100 + '%' });
         },
         init() {
             this.mainKvInit();
+            this.sliderInit();
         },
     };
 
