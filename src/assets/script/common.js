bfb878e44289bd1c17c4f455453d81cf37e68f41
baaayh <baaaayh@gmail.com>
2024-06-26 18:46:35 +0900

    sub

--- src/assets/script/common.js
+++ src/assets/script/common.js
@@ -12,5 +12,30 @@ $(function () {
         },
     };
 
-    return search.init();
+    const header = {
+        // toggleState() {
+        //     $('.header').toggleClass('active');
+        //     $('.dep2').stop().slideToggle();
+        // },
+        openHeader() {
+            $('.header').addClass('active');
+            $('.dep2').stop().slideDown(340);
+        },
+        closeHeader() {
+            $('.header').removeClass('active');
+            $('.dep2').stop().slideUp(340);
+        },
+        toggleItem() {
+            $(this).parents('li').toggleClass('active');
+        },
+        init() {
+            // $('#gnb').hover(this.toggleState);
+            $('.gnb').on('mouseenter', this.openHeader);
+            $('.gnb').on('mouseleave', this.closeHeader);
+            $('.dep2').hover(this.toggleItem).bind(this);
+        },
+    };
+
+    search.init();
+    header.init();
 });
